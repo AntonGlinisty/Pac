016c280885f876f46fc53a92a7c79d52b640492f
AntonGlinisty <glinistyi.ad@phystech.edu>
2022-05-02 19:54:18 +0300

    check_3

---
+++ pythonProject3/Food.py
@@ -0,0 +1,44 @@
+import pygame
+from Grid import Grid
+from Globals import Globals
+
+
+class Food(Grid):
+
+    def DrawFood(self, gridCord, button):
+        screenCord = Grid.GridToScreen(Grid, gridCord)
+        rect = pygame.Rect((screenCord[0], screenCord[1], Globals.cell_side, Globals.cell_side))
+
+        if button == 3:
+            pygame.draw.circle(Globals.screen, Globals.yellow, (screenCord[0] + Globals.cell_side//2,
+                                                                            screenCord[1] + Globals.cell_side//2), 5)
+        if button == 4:
+            pygame.draw.circle(Globals.screen, Globals.black, (screenCord[0] + Globals.cell_side//2,
+                                                                            screenCord[1] + Globals.cell_side//2), 5)
+
+    def DrawAllFood(self, GRID):
+        for line_counter in range(int(Globals.height//Globals.cell_side)):
+            for column_counter in range(int(Globals.width//Globals.cell_side)):
+                self.DrawFood((column_counter, line_counter), int(GRID.grid[line_counter][column_counter]))
+
+    def EndCheacker(self, foodcounter, GRID, FOOD, pacman, blinky, inky, clyde, marker, marker4, i, motion, score,
+                    marker7, marker8, marker9, marker10, marker11, marker12, live):
+        if foodcounter == 0:
+            Globals.screen.blit(Globals.sf, (0, 0))
+            GRID.DrawGrid()
+            FOOD.DrawAllFood(GRID)
+            pacman.rect.x = Globals.pac_spawnkord_x
+            pacman.rect.y = Globals.pac_spawnkord_y
+            blinky.rect.x = Globals.r_gh_spawnkord_x
+            blinky.rect.y = Globals.r_gh_spawnkord_y
+            clyde.rect.x = Globals.or_gh_spawnkord_x
+            clyde.rect.y = Globals.or_gh_spawnkord_y
+            inky.rect.x = Globals.bl_gh_spawnkord_x
+            inky.rect.y = Globals.bl_gh_spawnkord_y
+            i = 1
+            score = 0
+            live = 3
+            foodcounter = 157
+            motion = Globals.LEFT
+            marker = marker4 = marker7 = marker8 = marker9 = marker10 = marker11 = marker12 = True
+        return (marker, marker4, i, foodcounter, motion, score, marker7, marker8, marker9, marker10, marker11, marker12, live)
--- pythonProject3/Treatment.py
+++ pythonProject3/Treatment.py
@@ -1,22 +1,29 @@
 from Globals import Globals
 import pygame
+from Food import Food
+from Grid import Grid
 
 class Treatment():
 
-    def InputEvents(self, GRID, gridCord_LU, gridCord_RU, gridCord_LD,
-                                            gridCord_RD, pacman, mouse, potential, motion, button):
+    def InputEvents(self, GRID, FOOD, gridCord_LU, gridCord_RU, gridCord_LD,
+                                            gridCord_RD, pacman, U_block, I_block, potential, motion, button):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Globals.running = False
             elif event.type == pygame.KEYDOWN:
-                (motion, potential, mouse) = self.Keydown(event, motion, potential, GRID, gridCord_LU,
-                                                               gridCord_RU, gridCord_LD, gridCord_RD, pacman, mouse)
-            if mouse == 1 and event.type == pygame.MOUSEBUTTONDOWN:
-                self.Mousedown(event, GRID, button)
-        return (motion, potential, mouse)
+                (motion, potential, U_block, I_block) = self.Keydown(event, motion, potential,
+                                    GRID, gridCord_LU,gridCord_RU, gridCord_LD, gridCord_RD, pacman, U_block, I_block)
+            if U_block == 1 and event.type == pygame.MOUSEBUTTONDOWN and I_block == 0:
+                self.MouseDrawCell(event, GRID, button)
+                I_block = 0
+            elif I_block == 1 and event.type == pygame.MOUSEBUTTONDOWN and U_block == 0:
+                self.MouseDrawFood(event, GRID, button, FOOD)
+
+                U_block = 0
+        return (motion, potential, U_block, I_block)
 
     def Keydown(self, event, motion, potential, GRID, gridCord_LU,
-                                            gridCord_RU, gridCord_LD, gridCord_RD, pacman, mouse):
+                                              gridCord_RU, gridCord_LD, gridCord_RD, pacman, U_block,I_block):
         if event.key == pygame.K_LEFT:
             if (GRID.grid[gridCord_LU[1]][gridCord_LU[0] - 1] == 1
                     or GRID.grid[gridCord_LD[1]][gridCord_LD[0] - 1]):
@@ -49,18 +56,32 @@ class Treatment():
             else:
                 motion = Globals.DOWN
                 potential = Globals.DOWN
-        elif event.key == pygame.K_o:
+        (I_block, U_block) = self.MagicButtons(event, pacman, gridCord_LU, gridCord_RU, gridCord_LD, gridCord_RD,
+                                               U_block, I_block, GRID)
+        return (motion, potential, U_block, I_block)
+
+    def MagicButtons(self, event, pacman, gridCord_LU, gridCord_RU, gridCord_LD, gridCord_RD, U_block, I_block, GRID):
+        if event.key == pygame.K_o:
             print(gridCord_LU, gridCord_RU, gridCord_LD, gridCord_RD)
             print((pacman.rect.x, pacman.rect.y), (pacman.rect.x + 39, pacman.rect.y),
                   (pacman.rect.x, pacman.rect.y + 39), (pacman.rect.x + 39, pacman.rect.y + 39))
         elif event.key == pygame.K_u:
-            if mouse == 0:
-                mouse = 1
+            if U_block == 0:
+                U_block = 1
+                I_block = 0
             else:
-                mouse = 0
-        return (motion, potential, mouse)
+                U_block = 0
+        elif event.key == pygame.K_i:
+            if I_block == 1:
+                I_block = 0
+            else:
+                I_block = 1
+                U_block = 0
+        elif event.key == pygame.K_p:
+            Grid.GridSave(GRID)
+        return (I_block, U_block)
 
-    def Mousedown(self, event, GRID, button):
+    def MouseDrawCell(self, event, GRID, button):
         gridCord = GRID.ScreenToGrid(event.pos)
         if event.button == 1:
             GRID.grid[gridCord[1]][gridCord[0]] = 1
@@ -70,24 +91,36 @@ class Treatment():
             button = 0
         GRID.DrawCell(gridCord, button)
 
-    def Potential(self, potential, motion, GRID, gridCord_LU, gridCord_RU, gridCord_LD, gridCord_RD):
+    def MouseDrawFood(self, event, GRID, button, FOOD):
+        gridCord = GRID.ScreenToGrid(event.pos)
+        if event.button == 1:
+            if GRID.grid[gridCord[1]][gridCord[0]] != 1:
+                GRID.grid[gridCord[1]][gridCord[0]] = 3
+                button = 3
+                FOOD.DrawFood(gridCord, button)
+        elif event.button == 3:
+            if GRID.grid[gridCord[1]][gridCord[0]] != 1:
+                GRID.grid[gridCord[1]][gridCord[0]] = 0
+                screenCord = GRID.GridToScreen(gridCord)
+                rect = pygame.Rect((screenCord[0], screenCord[1], Globals.cell_side, Globals.cell_side))
+                pygame.draw.rect(Globals.screen, Globals.black, rect)
+
+    def Potential(self, potential, motion, GRID, gridCord_LU, gridCord_RU, gridCord_LD, gridCord_RD, pacman):
         if potential != motion:
-            if potential == Globals.LEFT and (GRID.grid[gridCord_LU[1]][gridCord_LU[0] - 1] == 0
-                                and GRID.grid[gridCord_LD[1]][gridCord_LD[0] - 1] == 0):
+            if potential == Globals.LEFT and (GRID.grid[gridCord_LU[1]][gridCord_LU[0] - 1] != 1
+                                and GRID.grid[gridCord_LD[1]][gridCord_LD[0] - 1] != 1):
                 motion = potential
-
-            if potential == Globals.RIGHT and (GRID.grid[gridCord_RU[1]][gridCord_RU[0] + 1] == 0
-                                and GRID.grid[gridCord_RD[1]][gridCord_RD[0] + 1] == 0):
+            if potential == Globals.RIGHT and (GRID.grid[gridCord_RU[1]][gridCord_RU[0] + 1] != 1
+                                and GRID.grid[gridCord_RD[1]][gridCord_RD[0] + 1] != 1):
                 motion = potential
-
-            if potential == Globals.UP and (GRID.grid[gridCord_LU[1] - 1][gridCord_LU[0]] == 0
-                                and GRID.grid[gridCord_RU[1] - 1][gridCord_RU[0]] == 0):
-                motion = potential
-
-            if potential == Globals.DOWN and (GRID.grid[gridCord_LD[1] + 1][gridCord_LD[0]] == 0
-                                and GRID.grid[gridCord_RD[1] + 1][gridCord_RD[0]] == 0):
-                motion = potential
-        return motion
+            if pacman.rect.x != Globals.width:
+                if potential == Globals.UP and (GRID.grid[gridCord_LU[1] - 1][gridCord_LU[0]] != 1
+                                    and GRID.grid[gridCord_RU[1] - 1][gridCord_RU[0]] != 1):
+                    motion = potential
+                if potential == Globals.DOWN and (GRID.grid[gridCord_LD[1] + 1][gridCord_LD[0]] != 1
+                                    and GRID.grid[gridCord_RD[1] + 1][gridCord_RD[0]] != 1):
+                    motion = potential
+        return (motion)
 
     def PacmanMovement(self, motion, pacman):
         if motion == Globals.LEFT:
@@ -100,75 +133,105 @@ class Treatment():
             pacman.rect.y += Globals.speed
 
     def PacmanTeleport(self, pacman):
-        if pacman.rect.x + 40 <= 0:
-            pacman.rect.x = 1200
-        elif pacman.rect.x >= 1200:
-            pacman.rect.x = -40
+        if pacman.rect.x + int(Globals.pacman_side) <= 0:
+            pacman.rect.x = int(Globals.width)
+        elif pacman.rect.x >= int(Globals.width):
+            pacman.rect.x = -int(Globals.pacman_side)
 
     def PacmanChangeDir(self, motion, direction, pacman):
         if motion == Globals.LEFT:
-            if direction == Globals.LEFT:
-                pass
-            elif direction == Globals.RIGHT:
-                pacman.image = pygame.transform.rotate(pacman.image, 180)
-                direction = Globals.LEFT
-            elif direction == Globals.UP:
-                pacman.image = pygame.transform.rotate(pacman.image, 90)
-                direction = Globals.LEFT
-            elif direction == Globals.DOWN:
-                pacman.image = pygame.transform.rotate(pacman.image, 270)
-                direction = Globals.LEFT
-        elif motion == Globals.RIGHT:
             if direction == Globals.RIGHT:
-                pass
-            elif direction == Globals.LEFT:
                 pacman.image = pygame.transform.rotate(pacman.image, 180)
-                direction = Globals.RIGHT
+            elif direction == Globals.UP:
+                pacman.image = pygame.transform.rotate(pacman.image, 90)
+            elif direction == Globals.DOWN:
+                pacman.image = pygame.transform.rotate(pacman.image, 270)
+        elif motion == Globals.RIGHT:
+            if direction == Globals.LEFT:
+                pacman.image = pygame.transform.rotate(pacman.image, 180)
             elif direction == Globals.DOWN:
                 pacman.image = pygame.transform.rotate(pacman.image, 90)
-                direction = Globals.RIGHT
             elif direction == Globals.UP:
                 pacman.image = pygame.transform.rotate(pacman.image, 270)
-                direction = Globals.RIGHT
         elif motion == Globals.UP:
-            if direction == Globals.UP:
-                pass
-            elif direction == Globals.DOWN:
-                pacman.image = pygame.transform.rotate(pacman.image, 180)
-                direction = Globals.UP
-            elif direction == Globals.RIGHT:
-                pacman.image = pygame.transform.rotate(pacman.image, 90)
-                direction = Globals.UP
-            elif direction == Globals.LEFT:
-                pacman.image = pygame.transform.rotate(pacman.image, 270)
-                direction = Globals.UP
-        elif motion == Globals.DOWN:
             if direction == Globals.DOWN:
-                pass
-            elif direction == Globals.UP:
                 pacman.image = pygame.transform.rotate(pacman.image, 180)
-                direction = Globals.DOWN
+            elif direction == Globals.RIGHT:
+                pacman.image = pygame.transform.rotate(pacman.image, 90)
+            elif direction == Globals.LEFT:
+                pacman.image = pygame.transform.rotate(pacman.image, 270)
+        elif motion == Globals.DOWN:
+            if direction == Globals.UP:
+                pacman.image = pygame.transform.rotate(pacman.image, 180)
             elif direction == Globals.LEFT:
                 pacman.image = pygame.transform.rotate(pacman.image, 90)
-                direction = Globals.DOWN
             elif direction == Globals.RIGHT:
                 pacman.image = pygame.transform.rotate(pacman.image, 270)
-                direction = Globals.DOWN
-        pacman.image.set_colorkey(Globals.white)
+        if motion != Globals.STOP:
+            direction = motion
         return (motion, direction)
+
+    def Counter(self, i):
+        i += 1
+        return i
+
+    def PacmanMouth(self, direction, pacman, counter):
+        if counter % Globals.mou_cl_freq == 0:
+            pacman.image = pygame.transform.scale(Globals.player_img_mouth, Globals.pacman_size)
+        if counter % Globals.mou_op_freq == 0 and not counter % Globals.mou_cl_freq == 0:
+            if direction == Globals.LEFT:
+                pacman.image = pygame.transform.scale(Globals.player_img_left, Globals.pacman_size)
+            elif direction == Globals.RIGHT:
+                pacman.image = pygame.transform.scale(Globals.player_img_right, Globals.pacman_size)
+            elif direction == Globals.UP:
+                pacman.image = pygame.transform.scale(Globals.player_img_up, Globals.pacman_size)
+            elif direction == Globals.DOWN:
+                pacman.image = pygame.transform.scale(Globals.player_img_down, Globals.pacman_size)
 
     def PacmanObstacle(self, motion, GRID, gridCord_LU, gridCord_RU, gridCord_LD, gridCord_RD, pacman):
         if motion == Globals.LEFT and (GRID.grid[gridCord_LU[1]][gridCord_LU[0] - 1] == 1
-                               or GRID.grid[gridCord_LD[1]][gridCord_LD[0] - 1] == 1) and pacman.rect.x % 40 == 0:
+                                        or GRID.grid[gridCord_LD[1]] [gridCord_LD[0] - 1] == 1) \
+                                                                    and pacman.rect.x % int(Globals.pacman_side) == 0:
             motion = Globals.STOP
-        if gridCord_RU[0] + 1 < 30:
+        if gridCord_RU[0] + 1 < int(Globals.width//Globals.cell_side):
             if motion == Globals.RIGHT and (GRID.grid[gridCord_RU[1]][gridCord_RU[0] + 1] == 1
-                                   or GRID.grid[gridCord_RD[1]][gridCord_RD[0] + 1] == 1) and pacman.rect.x % 40 == 0:
+                                        or GRID.grid[gridCord_RD[1]][gridCord_RD[0] + 1] == 1) \
+                                                                    and pacman.rect.x % int(Globals.pacman_side) == 0:
                 motion = Globals.STOP
         if motion == Globals.UP and (GRID.grid[gridCord_LU[1] - 1][gridCord_LU[0]] == 1
-                               or GRID.grid[gridCord_RU[1] - 1][gridCord_RU[0]] == 1) and pacman.rect.y % 40 == 0:
+                                        or GRID.grid[gridCord_RU[1] - 1][gridCord_RU[0]] == 1) \
+                                                                    and pacman.rect.y % int(Globals.pacman_side) == 0:
             motion = Globals.STOP
         if motion == Globals.DOWN and (GRID.grid[gridCord_LD[1] + 1][gridCord_LD[0]] == 1
-                               or GRID.grid[gridCord_RD[1] + 1][gridCord_RD[0]] == 1) and pacman.rect.y % 40 == 0:
+                                        or GRID.grid[gridCord_RD[1] + 1][gridCord_RD[0]] == 1) \
+                                                                    and pacman.rect.y % int(Globals.pacman_side) == 0:
             motion = Globals.STOP
         return motion
+
+    def PacmanEat(self, motion, GRID, pacman, gridCord_LU, gridCord_RU, gridCord_LD, gridCord_RD, score, foodcounter):
+        if gridCord_RU[0] + 1 < int(Globals.width // Globals.cell_side):
+            if motion == Globals.LEFT and GRID.grid[gridCord_LU[1]][gridCord_LU[0]] == 3:
+                if pacman.rect.x % int(Globals.cell_side) == int(Globals.cell_side) // 2 + 4:
+                    GRID.grid[gridCord_LU[1]][gridCord_LU[0]] = 0
+                    Food.DrawFood(Food, gridCord_LU, 4)
+                    score += Globals.bonus1
+                    foodcounter -= 1
+            if motion == Globals.RIGHT and GRID.grid[gridCord_RU[1]][gridCord_RU[0]] == 3:
+                if pacman.rect.x % int(Globals.cell_side) == int(Globals.cell_side) // 2 - 4:
+                    GRID.grid[gridCord_RU[1]][gridCord_RU[0]] = 0
+                    Food.DrawFood(Food, gridCord_RU, 4)
+                    score += Globals.bonus1
+                    foodcounter -= 1
+            if motion == Globals.UP and GRID.grid[gridCord_LU[1]][gridCord_LU[0]] == 3:
+                if pacman.rect.y % int(Globals.cell_side) == int(Globals.cell_side) // 2 + 4:
+                    GRID.grid[gridCord_LU[1]][gridCord_LU[0]] = 0
+                    Food.DrawFood(Food, gridCord_LU, 4)
+                    score += Globals.bonus1
+                    foodcounter -= 1
+            if motion == Globals.DOWN and GRID.grid[gridCord_LD[1]][gridCord_LD[0]] == 3:
+                if pacman.rect.y % int(Globals.cell_side) == int(Globals.cell_side) // 2 - 4:
+                    GRID.grid[gridCord_RD[1]][gridCord_RD[0]] = 0
+                    Food.DrawFood(Food, gridCord_RD, 4)
+                    score += Globals.bonus1
+                    foodcounter -= 1
+        return (GRID, score, foodcounter)
